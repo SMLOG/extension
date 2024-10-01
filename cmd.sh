bf4b3c0238b2debadaece24601af69eb33118075
test <smlog@github.com>
2024-10-01 12:36:48 +0800

    add p

--- cmd.sh
+++ cmd.sh
@@ -2,9 +2,16 @@
 
 #while ! git pull --rebase ; do echo 'lll';done;
 VERSION=`git branch --show-current`
-mkdir -p ../testdemo/${VERSION}
-(node updateVersion.js && npm run build2 -- --dest="../testdemo/${VERSION}" --no-clean )&&echo 'build done'
+
+dest='testdemo'
+
+if [[ $VERSION == p* ]]; then
+    dest='p'
+fi
+
+mkdir -p ../${dest}/${VERSION}
+(node updateVersion.js && npm run build2 -- --dest="../${dest}/${VERSION}" --no-clean )&&echo 'build done'
 sed -E -i  "s|<base href=\"[^\"]*?\">|<base href=\"${VERSION}/\">|" ../testdemo/index.html
 #cp -a ../my-extension3/dist/* .
-(cd ../testdemo && git add . && git commit -am 'update' && while ! git push ; do echo 'lll';done;)
+(cd ../${dest} && git add . && git commit -am 'update' && while ! git push ; do echo 'lll';done;)
 echo 'done';
